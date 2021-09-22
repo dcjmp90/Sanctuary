165026c2c1983a3e8e4b3b1fbce5d6c93198807c
Jon Perry <26749062+dcjmp90@users.noreply.github.com>
2021-09-21 20:52:59 -0500

    Update items.py

--- components/modules/items.py
+++ components/modules/items.py
@@ -40,7 +40,7 @@ class RuneWordItem(BaseItem):
         #TODO
         raise NotImplementedError('You must implement this in subclass!')
 
-    def _get_socket_requirement(self, specs)"
+    def _get_socket_requirement(self, specs):
         """get the number of sockets required"""
 
     def _get_level_required(self, specs):
