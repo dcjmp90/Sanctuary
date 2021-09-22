d21c67a9916da2ab743b6cc275c8bc0eb213dc9f
Jon Perry <dcjmp90@gmail.com>
2021-09-22 22:00:58 +0000

    missing enumerate, added

--- components/modules/items.py
+++ components/modules/items.py
@@ -96,7 +96,7 @@ class RuneWordItem(BaseItem):
     
     def _add_stats(self, specs):
         """get the stats for the item"""
-        for idx, priority in range(5,len(specs)*5+1,3)):
+        for idx, priority in enumerate(range(5,len(specs)*5+1,3)):
             self.build_attrs[priority] = '\t\t'+specs[idx]+'\n'
     
     def _add_item_type(self, specs):
